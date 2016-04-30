5e60e5275b16d3ae547cdd49f482487242a8a9a3
Daniel Savage <dansvg@gmail.com>
2016-04-29 22:28:14 -0700

    Key and lock logic

--- src/main.js
+++ src/main.js
@@ -22,14 +22,22 @@ var Patterns = {
   goal: [1, 0, 1, 1, 0, 0],
   bp: [1, 1, 0, 0, 1, 1],
   c: [0, 1, 0, 0, 1, 1],
+  lock: [1, 1, 0, 0, 0, 1],
+  key: [0, 1, 0, 0, 1, 0],
 };
 
 // Hash for mapping map tiles to patterns
 var MapKeyToPattern = {};
 MapKeyToPattern['wa'] = 'wall';
 MapKeyToPattern[0] = 'open';
+MapKeyToPattern['st'] = 'open';
 MapKeyToPattern['gl'] = 'goal';
+MapKeyToPattern['lo'] = 'lock';
+MapKeyToPattern['ke'] = 'key';
 
+// Maps are represented as two-dimensional arrays.
+// The game has no bounds checking, so be sure to wrap everyting
+// around with walls or the game will crash.
 var Maps = [
 {
   data: [['wa', 'wa', 'wa', "wa", "wa", "wa", "wa"],
@@ -49,6 +57,43 @@ var Maps = [
          ["wa", "wa", "wa", "wa", "wa", "wa", "wa", "wa"],
   ],
 },
+{
+  data: [['wa', "wa", 'wa', "wa", "wa", "wa", "wa", "wa"],
+         ["wa",    0,    0, "wa", "wa", "wa", "wa", "wa"],
+         ["wa",    0,    0, "wa",    0, "wa", "wa", "wa"],
+         ["wa", "st",    0, "wa",    0, "wa", "wa", "wa"],
+         ["wa",    0, "wa", "wa",    0, "wa", "wa", "wa"],
+         ["wa",    0,    0,    0,    0, "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "wa",    0, "wa", "wa", "wa"],
+         ["wa", "wa", "gl",    0,    0, "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "wa", "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "wa", "wa", "wa", "wa", "wa"],
+  ],
+},
+{
+  data: [['wa', "wa", 'wa', "wa", "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa",    0,    0,    0,    0, "wa"],
+         ["wa", "wa", "wa", "wa",    0, "wa", "st", "wa"],
+         ["wa", "wa", "wa",    0,    0,    0, "wa", "wa"],
+         ["wa", "wa", "wa", "wa",    0, "wa", "wa", "wa"],
+         ["wa", "wa", "wa",    0,    0,    0, "gl", "wa"],
+         ["wa", "wa", "wa", "wa", "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "wa", "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "wa", "wa", "wa", "wa", "wa"],
+  ],
+},
+{
+  data: [['wa', "wa", 'wa', "wa", "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "gl", "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa",    0, "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "lo", "wa", "wa", "wa", "wa"],
+         ["wa",    0,    0,    0, "wa", "wa", "wa", "wa"],
+         ["wa", "ke", "wa",    0, "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa",    0, "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "st", "wa", "wa", "wa", "wa"],
+         ["wa", "wa", "wa", "wa", "wa", "wa", "wa", "wa"],
+  ],
+},
 ];
 
 var View = function () {
@@ -64,6 +109,7 @@ var View = function () {
   this.currentLevel = null;
   this.currentX = 0;
   this.currentY = 0;
+  this.hasKey = false;
 };
 View.prototype.init = function (levelIndex) {
   // game scaling
@@ -94,6 +140,7 @@ View.prototype.init = function (levelIndex) {
   this.facing = Constants.Directions.North;
   this.currentX = 0;
   this.currentY = 0;
+  this.hasKey = false;
 };
 View.prototype.preload = function () {
   this.game.load.audio('hi', 'asset/sfx/hi.wav');
@@ -121,7 +168,10 @@ View.prototype.create = function () {
   var forwardKey = this.game.input.keyboard.addKey(Phaser.KeyCode.UP);
   forwardKey.onDown.add(function () {
     this.stepForward();
-    this.showFacingObject();
+  }, this);
+  var backwardKey = this.game.input.keyboard.addKey(Phaser.KeyCode.DOWN);
+  backwardKey.onDown.add(function () {
+    this.stepBackward();
   }, this);
   var rightKey = this.game.input.keyboard.addKey(Phaser.KeyCode.RIGHT);
   rightKey.onDown.add(function () {
@@ -188,7 +238,12 @@ View.prototype.getFacingForwardMapKey = function () {
   return Maps[this.currentLevel].data[targetY][targetX];
 };
 View.prototype.showFacingObject = function () {
-  this.playPattern(MapKeyToPattern[this.getFacingForwardMapKey()]);
+  // don't play the key pattern if we're holding the key
+  if (this.getFacingForwardMapKey() === 'ke' && this.hasKey === true) {
+    this.playPattern('open');
+  } else {
+    this.playPattern(MapKeyToPattern[this.getFacingForwardMapKey()]);
+  }
 };
 View.prototype.stepForward = function () {
   var targetX = this.currentX;
@@ -209,9 +264,53 @@ View.prototype.stepForward = function () {
     break;
   }
 
-  if (Maps[this.currentLevel].data[targetY][targetX] !== "wa") {
+  if (!(Maps[this.currentLevel].data[targetY][targetX] === "wa" || (Maps[this.currentLevel].data[targetY][targetX] === "lo" && this.hasKey === false))) {
     this.currentX = targetX;
     this.currentY = targetY;
+
+    this.showFacingObject();
+  } else {
+    return;
+  }
+
+  if (Maps[this.currentLevel].data[targetY][targetX] === "ke" && this.hasKey === false) {
+    this.hasKey = true;
+  }
+
+  if (Maps[this.currentLevel].data[targetY][targetX] === "gl") {
+    this.game.state.start('View', true, false, (this.currentLevel + 1) % Maps.length);
+  }
+};
+View.prototype.stepBackward = function () {
+  var targetX = this.currentX;
+  var targetY = this.currentY;
+
+  switch (this.facing) {
+    case Constants.Directions.North:
+    targetY++;
+    break;
+    case Constants.Directions.East:
+    targetX--;
+    break;
+    case Constants.Directions.South:
+    targetY--;
+    break;
+    case Constants.Directions.West:
+    targetX++;
+    break;
+  }
+
+  if (!(Maps[this.currentLevel].data[targetY][targetX] === "wa" || (Maps[this.currentLevel].data[targetY][targetX] === "lo" && this.hasKey === false))) {
+    this.currentX = targetX;
+    this.currentY = targetY;
+    
+    this.showFacingObject();
+  } else {
+    return;
+  }
+
+  if (Maps[this.currentLevel].data[targetY][targetX] === "ke" && this.hasKey === false) {
+    this.hasKey = true;
   }
 
   if (Maps[this.currentLevel].data[targetY][targetX] === "gl") {
@@ -223,6 +322,7 @@ View.prototype.turn = function (angles) {
 };
 
 // debug map exploration
+/*
 View.prototype.render = function () {
   for (var i = 0; i < Maps[this.currentLevel].data.length; i++) {
     for (var j = 0; j < Maps[this.currentLevel].data[i].length; j++) {
@@ -230,6 +330,10 @@ View.prototype.render = function () {
         this.game.debug.geom(new Phaser.Rectangle(j * 3, i * 3, 3, 3));
       } else if (Maps[this.currentLevel].data[i][j] === "gl") {
         this.game.debug.geom(new Phaser.Rectangle(j * 3, i * 3, 3, 3), 'pink');
+      } else if (Maps[this.currentLevel].data[i][j] === "lo") {
+        this.game.debug.geom(new Phaser.Rectangle(j * 3, i * 3, 3, 3), 'green');
+      } else if (Maps[this.currentLevel].data[i][j] === "ke" && this.hasKey === false) {
+        this.game.debug.geom(new Phaser.Rectangle(j * 3, i * 3, 3, 3), 'light green');
       }
     }
   }
@@ -250,11 +354,12 @@ View.prototype.render = function () {
     break;
   }
 };
+*/
 
 var main = function () {
   var game = new Phaser.Game(32, 32);
   game.state.add('View', View, false);
-  game.state.start('View', true, false, 0);
+  game.state.start('View', true, false, 4);
 };
 
 
